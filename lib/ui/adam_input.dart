54a2eb01a35fa4e990915daece81b5a31fdb396e
AdamChen <mnre2003@yahoo.com.tw>
2019-02-01 10:13:21 +0800

    Learn to build stepper widget

--- lib/ui/adam_input.dart
+++ lib/ui/adam_input.dart
@@ -23,11 +23,79 @@ class _MyTextInputState extends State<MyTextInput> {
       appBar: AppBar(title: Text("Demo input"),),
       body: Container(
         padding: const EdgeInsets.all(20.0),
-        child: _buildEditInput(),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              _buildEditInput(),
+              Divider(height: 5, color: Colors.red,),
+              _buildStep(),
+            ],
+          ),
+        )
       )
 
     );
   }
+
+  List<Step> steps = [
+     Step(
+       title: Text("Start"),
+       content: Text("Initialize data..."),
+     ),
+     Step(
+       title: Text("End"),
+       content: Text("Get response..."),
+     ),
+  ];
+  // record step index
+  int _index = 0;
+  Widget _buildStep() => Container(
+    color: Colors.yellow,
+    child: Stepper(
+        steps: steps.toList(),
+        currentStep: _index,
+        onStepTapped: (index) {
+          setState(() {
+            _index = index;
+          });
+        },
+//        onStepContinue: () {
+//          Fluttertoast.showToast(msg: "Hit Continue...");
+//        },
+//        onStepCancel: () {
+//          Fluttertoast.showToast(msg: "Hit Cancel...");
+//        },
+        // Customize button of the step
+        controlsBuilder: (context, {onStepContinue, onStepCancel}) {
+          return Container(
+            child: Row(
+              children: <Widget>[
+                FlatButton(
+                  child: Text("Go..."),
+                  color: Colors.blue,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+                  onPressed: () {
+                    Fluttertoast.showToast(msg: "Hit Go...");
+                  },
+                ),
+                SizedBox(width: 5.0,),
+                FlatButton(
+                  child: Text("Stop..."),
+                  color: Colors.blue,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+                  onPressed: () {
+                    Fluttertoast.showToast(msg: "Hit Stop...");
+                  },
+                ),
+              ],
+            ),
+          );
+        },
+
+    ),
+  );
 
 
   Widget _buildEditInput() => Center(
@@ -43,7 +111,7 @@ class _MyTextInputState extends State<MyTextInput> {
           ),
 
         ),
-        SizedBox(height: 10.0,),
+        SizedBox(height: 5.0,),
         ButtonBar(
           alignment: MainAxisAlignment.end,
           children: <Widget>[
